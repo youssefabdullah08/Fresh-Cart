8d6f487acd42c703f30c9391bbea53eb1ed8d774
youssefabdullah08 <youssefabdullah.583002@gmail.com>
2025-12-18 11:02:35 +0200

    feat: Implement authentication guard, add authentication navigation bar, and create a responsive header slider component.

--- src/app/gards/auth.guard.ts
+++ src/app/gards/auth.guard.ts
@@ -9,7 +9,7 @@ let router =inject(Router)
     return true;
   }
   else{
-    alert("you can't go to home after log in ")
+    alert("you can't make any action after log in ")
    router.navigate(['/login'])
    return false;
   }
